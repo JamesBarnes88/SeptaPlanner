66aa97971b5d67783e0aa2708557ef42e7af909c
mike <mike@10.0.0.31>
2014-03-07 18:26:33 -0500

    1st Commit

--- src/com/drexel/septaplanner/SeptaActivity.java
+++ src/com/drexel/septaplanner/SeptaActivity.java
@@ -2,8 +2,14 @@ package com.drexel.septaplanner;
 
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SeptaActivity extends Activity {
 
@@ -11,6 +17,42 @@ public class SeptaActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_septa);
+
+		
+		/*Buttons on Activity*/
+		Button buttonBuses = (Button) findViewById(R.id.button_septa_buses);
+		Button buttonTrains = (Button) findViewById(R.id.button_septa_trains);
+		Button buttonRegRail= (Button) findViewById(R.id.button_septa_regrail);
+		
+		buttonBuses.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(SeptaActivity.this, TripActivity.class);
+				startActivity(i);
+			}
+		});
+		buttonTrains.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(SeptaActivity.this, SeptaActivity.class);
+				startActivity(i);
+			}
+		});
+		buttonRegRail.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(SeptaActivity.this, RecentTripsActivity.class);
+				startActivity(i);
+			}
+		});
+		
+		/*Set buttons Colors
+		buttonBuses.getBackground().setColorFilter(0xFFB22222,PorterDuff.Mode.MULTIPLY);
+		buttonTrains.getBackground().setColorFilter(0xffff4629,PorterDuff.Mode.MULTIPLY);
+		buttonRegRail.getBackground().setColorFilter(0xff0000ff,PorterDuff.Mode.MULTIPLY);*/
 	}
 
 	@Override
