2a2beda38079c6ec413ed85ca179c39b1e233e6f
James Barnes <evybodyisno1@gmail.com>
2014-03-07 19:09:01 -0500

    mpa frag now does wat it did in the septa viewer app

--- src/com/drexel/septaplanner/MyMapFrag.java
+++ src/com/drexel/septaplanner/MyMapFrag.java
@@ -1,31 +1,77 @@
 package com.drexel.septaplanner;
 
+import java.util.ArrayList;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapLoadedCallback;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.LatLngBounds.Builder;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MyMapFrag extends FragmentActivity {
 
-	String tag="mymapfrag";
+	String tag = "mymapfrag";
+	LatLngBounds bounds;
+
+	public GoogleMap mMap;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map_fragment);
-		
-		GoogleMap mMap;
-		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		// get arraylist passed into intent
+		Intent i = getIntent();
+		ArrayList<septaTrain> trains = i.getParcelableArrayListExtra("trains");
 
-		LatLng train= new LatLng(39.94710, -75.19120);
-		Marker m= mMap.addMarker(new MarkerOptions().position(train).title("hello world train"));
-		mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(train, 14), 3000, null);
-		m.showInfoWindow();
+		// make map object
+		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+
+		// set markers on map
+		ArrayList<Marker> markers = new ArrayList<Marker>();
+		LatLng l;
+		Builder bBounds = new LatLngBounds.Builder();
+		
+		//get bounds for the group of trains and also the add markers on 
+		//map for those trains
+		for (int k = 0; k < trains.size(); k++) {
+			l = trains.get(k).getLatlong();
+			if (l.latitude != 0 && l.longitude != 0) {
+				bBounds.include(l);
+				markers.add(mMap.addMarker(new MarkerOptions().position(l)
+						.title("#" + trains.get(k).getTrainNum())));
+			}
+		}
+		if (markers.size() == 0){
+			//create bounds for philadelphia septa area
+			bBounds.include(new LatLng(39.863371,-75.411728));
+			bBounds.include(new LatLng(40.295894,-74.681652));
+
+			Toast.makeText(
+					MyMapFrag.this,
+					"There are no trains with gps coordinates on this route",
+					Toast.LENGTH_LONG).show();
+		}
+			bounds = bBounds.build();
+
+		mMap.setOnMapLoadedCallback(new OnMapLoadedCallback() {
+			@Override
+			public void onMapLoaded() {
+				mMap.animateCamera(
+						CameraUpdateFactory.newLatLngBounds(bounds, 100), 1500,
+						null);
+			}
+		});
 	}
 
 	@Override
